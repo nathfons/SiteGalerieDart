7878b7b03bbdf82dfc477cfc3801e45b3c9c1eb0
alexandra <alexandra.rubleva@viacesi.fr>
2020-01-09 09:01:52 +0100

    page admin

--- galerieWeb/src/Controller/AdminController.php
+++ galerieWeb/src/Controller/AdminController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CadreRepository;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(CadreRepository $cadreRepository)
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'main'=>'Accueil','cadre'=>'Cadre',
+            'cadres' => $cadreRepository->findAll(),
         ]);
     }
+    
 }
