73c58065bc586fe9460727da85bc0712c79945c4
Nathalie <nathalie.fons@gmail.com>
2020-02-06 23:31:13 +0100

    bdd

--- galerieWeb/src/DataFixtures/AppFixtures.php
+++ galerieWeb/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\TypePaiement;
+use App\Entity\Typelivraison;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,25 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $modePaiement = new TypePaiement();
+        $modePaiement->setNomtypepaiement("Carte Bleue");
+        $manager->persist($modePaiement);
+
+        $modePaiement = new TypePaiement();
+        $modePaiement->setNomtypepaiement("PayPal");
+        $manager->persist($modePaiement);
+
+        $modeLivraison = new Typelivraison();
+        $modeLivraison->setNomtypelivraison("Chronopost");
+        $modeLivraison->setPrixlivraison("5.0");
+        $modeLivraison->setNomtypelivraison("Chronopost");
+        $manager->persist($modeLivraison);
+
+        $modeLivraison = new Typelivraison();
+        $modeLivraison->setNomtypelivraison("Courrier suivi");
+        $modeLivraison->setPrixlivraison("2.5");
+        $manager->persist($modeLivraison);
 
         $manager->flush();
     }
