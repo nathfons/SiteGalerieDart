7c968a20fd2c2b896869420ed4f1db983639c634
Nathalie <nathalie.fons@gmail.com>
2020-02-11 00:59:12 +0100

    update user

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -69,8 +69,11 @@ class Utilisateur implements UserInterface
     }
 
     public function getRoles(): ?array
-    {
-        return $this->roles;
+    { $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): self
