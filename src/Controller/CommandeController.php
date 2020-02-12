dd7348a0852f0e1305b65e7ca6d534a847cf54a6
Nathalie <nathalie.fons@gmail.com>
2020-02-12 02:28:09 +0100

    test

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -96,7 +96,7 @@ class CommandeController extends AbstractController
        
             if ($form->isSubmitted() && $form->isValid()) {
                 $this->commande->setReferencecommande('REF-C'.$date->format("Y-m-d\TH:i:sP").$client->getNom());
-
+            /*
            
                 foreach($client->getAdresses() as $adresse){
                     $adresse->setIdClient($client);
@@ -105,14 +105,13 @@ class CommandeController extends AbstractController
                 $commande->setIdClient($client);
                 $this->getDoctrine()->getManager()->persist($client);
                 $this->getDoctrine()->getManager()->persist($this->commande);
-                if( $this->commande->getIdAdresse()!=null){
+                if( $this->commande->getIdAdresse()!=null){*/
                             $this->getDoctrine()->getManager()->flush();
                             $this->commande=null;
                             return $this->redirectToRoute('commande_paiement');
-                        }else{
+                       /* }else{
                             //Ajouter warning erreur pour utilisateur - adresse pas renseignée
-                            $this->getDoctrine()->getManager()->flush($client);
-                        }
+                        }*/
                 
             }
            
